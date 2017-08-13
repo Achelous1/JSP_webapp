dfcac282cd54d444d62af1609181aa233d92c99b
DESKTOP-6HRGNRL\korea <korean_js@naver.com>
2017-08-13 20:21:33 +0900

    폴더관리 및 레이아웃 수정

---
+++ JSP_webapp/js/script.js
@@ -0,0 +1,28 @@
+var app = angular.module("layout", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/main", {
+        templateUrl : "route/main.html"
+    })
+    .when("/myMatchList", {
+        templateUrl : "route/myMatchList.html"
+    })
+    .when("/recommendations", {
+        templateUrl : "route/recommendations.html"
+    })
+    .when("/reviews", {
+        templateUrl : "route/reviews.html"
+    })
+    .when("/myPage", {
+        templateUrl : "route/mypage.html"
+    })
+    .when("/myMatchList", {
+        templateUrl : "route/myMatchList.html"
+    })
+    .when("/signup", {
+        templateUrl : "route/signup.html"
+    })
+    otherwise({
+        redirectTo : "route/main.html:"
+    });
+});
