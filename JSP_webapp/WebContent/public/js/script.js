e0e1ff7a8ba5076ae2e03c0d68fbc3e4f1cc5c83
DESKTOP-6HRGNRL\korea <korean_js@naver.com>
2017-08-15 15:32:38 +0900

    앵귤러 라우팅 추가

---
+++ JSP_webapp/WebContent/public/js/script.js
@@ -0,0 +1,32 @@
+var app = angular.module('layout', ['ngRoute']);
+app.config(function($routeProvider, $locationProvider, $httpProvider){
+  $routeProvider
+  .when('/', {
+      templateUrl: 'JSP_webapp/public/route/main.html'
+  })
+  .when('/myMatchList', {
+      templateUrl: 'JSP_webapp/public/route/myMatchList.html'
+  })
+  .when('/myMatchList/:talkStranger', {
+      templateUrl: 'public.route/talkStranger.html'
+  })
+  .when('/recommendations', {
+      templateUrl: 'public/route/recommendations.html'
+  })
+  .when('/reviews', {
+      templateUrl: 'public/route/reviews.html'
+  })
+  .when('/signup', {
+      templateUrl: 'public/route/signup.html'
+  })
+  .when('/boardWrite', {
+      templateUrl: 'public/route/boardWrite.html'
+  })
+  .when('/myPage', {
+      templateUrl: 'public/route/myPage.html'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+  $locationProvider.html5Mode(true);
+});
